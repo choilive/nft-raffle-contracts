5bdaae27c395a2ad3c9f90c4b2f4d4fa977f389f
Zsofie <zsofia@linumlabs.com>
2022-05-26 17:27:41 +0200

    added constructor

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -5,31 +5,44 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Raffle is Ownable, AccessControl, ReentrancyGuard {
-    // TODO - implement access control!
-    address public DAOWallet;
-    uint256 public raffleCount;
-    IERC20 public USDC;
-    uint256 public totalDonations;
-    // -------------------------------------------------------------
-    // STORAGE
-    // --------------------------------------------------------------
-    struct Raffle {
-        address nftContract; // address of NFT contract
-        uint256[] tokenIDs;
-        uint256 startTime;
-        uint256 endTime;
-        uint256 minimumDonationAmount;
-        bool isActive;
-    }
-    mapping(uint256 => Raffle) public raffles;
-    mapping(uint256 => bool) public nftsClaimed;
-    mapping(uint256 => address) private highestDonor;
-    mapping(address => uint256) public donationPerAddress;
-    // // --------------------------------------------------------------
-    // // EVENTS
-    // // --------------------------------------------------------------
+  // TODO - implement access control!
+  address public DAOWallet;
+  uint256 public raffleCount;
+  IERC20 public USDC;
+  uint256 public totalDonations;
+  // -------------------------------------------------------------
+  // STORAGE
+  // --------------------------------------------------------------
+  struct Raffle {
+    address nftContract; // address of NFT contract
+    uint256[] tokenIDs;
+    uint256 startTime;
+    uint256 endTime;
+    uint256 minimumDonationAmount;
+    bool isActive;
+  }
+  mapping(uint256 => Raffle) public raffles;
+  mapping(uint256 => bool) public nftsClaimed;
+  mapping(uint256 => address) private highestDonor;
+  mapping(address => uint256) public donationPerAddress;
+  // // --------------------------------------------------------------
+  // // EVENTS
+  // // --------------------------------------------------------------
 
-    event RaffleCreated(uint256 startTime, uint256 endTime);
-    event DonationPlaced(address from, uint256 raffleId, uint256 amount);
-    event DAOWalletAddressSet(address walletAddress);
+  event RaffleCreated(uint256 startTime, uint256 endTime);
+  event DonationPlaced(address from, uint256 raffleId, uint256 amount);
+  event DAOWalletAddressSet(address walletAddress);
+
+  // --------------------------------------------------------------
+  // CUSTOM ERRORS
+  // --------------------------------------------------------------
+
+  // --------------------------------------------------------------
+  // CONSTRUCTOR
+  // --------------------------------------------------------------
+
+  constructor(address _usdc) {
+    USDC = IERC20(_usdc);
+    USDC.approve(address(this), type(uint256).max);
+  }
 }
