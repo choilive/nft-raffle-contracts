7a2a916d4752c59748b436e1872feafd7acd0c59
Zsofie <zsofia@linumlabs.com>
2022-06-02 14:57:23 +0200

    function onERC1155Received added to contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-contract Raffle is Ownable, AccessControl, ReentrancyGuard, IERC1155Receiver {
+contract Raffle is Ownable, AccessControl, ReentrancyGuard {
   // TODO - implement access control!
   address public DAOWallet;
   address public nftAuthorWallet;
@@ -266,9 +266,21 @@ contract Raffle is Ownable, AccessControl, ReentrancyGuard, IERC1155Receiver {
     return singleDonationsCount;
   }
 
+  function onERC1155Received(
+    address operator,
+    address from,
+    uint256 id,
+    uint256 value,
+    bytes memory data
+  ) external pure returns (bytes4) {
+    // 0xf23a6e61 = bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)")
+    return 0xf23a6e61;
+  }
+
   // --------------------------------------------------------------
   // INTERNAL STATE-MODIFYING FUNCTIONS
   // --------------------------------------------------------------
+
   function _calcRandomDonor(uint256 raffleID) internal view returns (address) {
     uint256 amountOfDonors = donorsArrayPerCycle[raffleID].length;
 
