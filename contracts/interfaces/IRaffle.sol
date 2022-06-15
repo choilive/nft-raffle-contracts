22074a0d96617b9212c77508e209704892106295
Zsofie <zsofia@linumlabs.com>
2022-06-15 22:28:02 +0200

    added TokenRewards.sol & IRaffle.sol

---
+++ contracts/interfaces/IRaffle.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.8.11;
+
+interface IRaffle {
+    function getTotalDonationsPerCycle(uint256 raffleID)
+        external
+        view
+        returns (uint256);
+
+    function getDonorsPerCycle(uint256 raffleID)
+        external
+        view
+        returns (address[] memory);
+
+    function getTotalDonationPerAddressPerCycle(
+        uint256 raffleID,
+        address account
+    ) external view returns (uint256);
+
+    function getHighestDonationPerCycle(uint256 raffleID)
+        external
+        view
+        returns (uint256);
+
+    function getTopDonor(uint256 raffleID) external view returns (address);
+}
