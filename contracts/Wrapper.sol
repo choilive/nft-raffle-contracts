7e3f5bb423eee6614529795c65c65ee5193e8d7f
Zsofie <zsofia@linumlabs.com>
2022-07-13 17:31:12 +0200

    started contract skeleton

--- contracts/Wrapper.sol
+++ contracts/Wrapper.sol
@@ -1,3 +1,36 @@
 pragma solidity 0.8.11;
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Wrapper is AccessControl {}
+contract Wrapper is AccessControl {
+
+  struct Organization {
+    string name, // should we register name on-chain
+    address[] contractsDeployed,
+  }
+
+
+  // --------------------------------------------------------------
+  // EVENTS
+  // --------------------------------------------------------------
+
+event OrganizationCreated(string name);
+event RaffleModuleAdded(uint organizationID,address module);
+event TreasuryModuleAdded(uint organizationID,address module);
+
+   // --------------------------------------------------------------
+  // PUBLIC FUNCTIONS
+  // --------------------------------------------------------------
+
+
+  function registerOrganization() public
+  function createNewRaffleContract() public{}
+  function createNewTreasuryContract() public {}
+
+    // --------------------------------------------------------------
+  // ONLY OWNER FUNCTIONS
+  // --------------------------------------------------------------
+
+  function setFees(uint fee) public onlyOwner returns(uint){
+    // set organization fee going back to protocol
+  }
+}
