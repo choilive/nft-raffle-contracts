eb0d9231a8621bb3babebdb08b482e084059b6cf
Zsofie <zsofia@linumlabs.com>
2022-07-15 22:28:43 +0200

    added createOrganization function logic

--- contracts/protocol/Wrapper.sol
+++ contracts/protocol/Wrapper.sol
@@ -2,62 +2,75 @@ pragma solidity 0.8.11;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wrapper is Ownable {
+    uint256 public constant SCALE = 10000; // Scale is 10 000
+    uint256 public protocolFee;
+    uint256 public organizationCount;
 
-uint256 public constant SCALE = 10000; // Scale is 10 000
-uint256 public protocolFee;
+    struct Organization {
+        string name; //TODO do we need this on chain?
+        address[] contractsDeployed;
+        address walletAddress; // wallet address given by organization
+        uint256 organizationID;
+    }
 
-
-
-  struct Organization {
-    string name, // should we register name on-chain
-    address[] contractsDeployed,
-    address walletAddress,
-    uint organizationID // organization centracl wallet address
-  }
-
-
-  // --------------------------------------------------------------
-  // EVENTS
-  // --------------------------------------------------------------
-
-event OrganizationCreated(string name);
-event RaffleModuleAdded(uint organizationID,address module);
-event TreasuryModuleAdded(uint organizationID,address module);
-
-  // --------------------------------------------------------------
-  // EVENTS
-  // --------------------------------------------------------------
-
-  error FeeOutOfRange();
+    mapping(uint256 => Organization) organizations;
 
     // --------------------------------------------------------------
-  // CONSTRUCTOR
-  // --------------------------------------------------------------
+    // EVENTS
+    // --------------------------------------------------------------
 
-  constructor() {
-
-  }
-   // --------------------------------------------------------------
-  // PUBLIC FUNCTIONS
-  // --------------------------------------------------------------
-
-
-  function createOrganization() public {
-
-  }
-  function createNewRaffleContract() public{}
-  function createNewTreasuryContract() public {}
+    event OrganizationCreated(uint256 id, address walletAddress);
+    event RaffleModuleAdded(uint256 organizationID, address module);
+    event TreasuryModuleAdded(uint256 organizationID, address module);
 
     // --------------------------------------------------------------
-  // ONLY OWNER FUNCTIONS
-  // --------------------------------------------------------------
+    // EVENTS
+    // --------------------------------------------------------------
 
-  function setProtocolFee(uint _protocolFee) public onlyOwner returns(uint){
-         require(
-            _protocolFee < SCALE
-        ) revert FeeOutOfRange();
+    error FeeOutOfRange();
+    error NoZeroAddressAllowed();
 
+    // --------------------------------------------------------------
+    // CONSTRUCTOR
+    // --------------------------------------------------------------
+
+    constructor() {}
+
+    // --------------------------------------------------------------
+    // PUBLIC FUNCTIONS
+    // --------------------------------------------------------------
+
+    function createOrganization(Organization memory _organization)
+        public
+        returns (uint256)
+    {
+        if (_organization.walletAddress == address(0))
+            revert NoZeroAddressAllowed();
+        organizationCount++;
+        _organization.organizationID = organizationCount;
+        organizations[organizationCount] = _organization;
+        emit OrganizationCreated(
+            _organization.organizationID,
+            _organization.walletAddress
+        );
+        return organizationCount;
+    }
+
+    function createNewRaffleContract() public {}
+
+    function createNewTreasuryContract() public {}
+
+    // --------------------------------------------------------------
+    // ONLY OWNER FUNCTIONS
+    // --------------------------------------------------------------
+
+    function setProtocolFee(uint256 _protocolFee)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        if (_protocolFee < SCALE) revert FeeOutOfRange();
         protocolFee = _protocolFee;
         return protocolFee;
-  }
+    }
 }
