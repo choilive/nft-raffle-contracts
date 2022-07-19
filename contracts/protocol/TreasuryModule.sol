f1de2371689cbc8b9fec8e5af37882b1c336a1cf
Zsofie <zsofia@linumlabs.com>
2022-07-19 21:49:39 +0200

    added Aave deposit/withdrawal to treasuryModule

--- contracts/protocol/TreasuryModule.sol
+++ contracts/protocol/TreasuryModule.sol
@@ -7,6 +7,7 @@ import "contracts/interfaces/AaveIntegration/IAToken.sol";
 import "contracts/interfaces/AaveIntegration/IAaveIncentivesController.sol";
 import "contracts/interfaces/IWrapper.sol";
 
+// TODO need to check who is the owner if you deploy it from a wrapper!!!!!!!
 contract TreasuryModule is Ownable {
     uint256 public treasuryBalance;
     uint256 constant SCALE = 10000; // Scale is 10 000
@@ -32,11 +33,17 @@ contract TreasuryModule is Ownable {
     event USDCMovedFromTreasuryToAave(uint256 amount);
     event ProtocolFeesReduced(uint256 amount);
 
+    // raffleID => total amount of donations
+    mapping(uint256 => uint256) totaldonationsPerRaffle;
     // --------------------------------------------------------------
     // CUSTOM ERRORS
     // --------------------------------------------------------------
 
     error ZeroAddressNotAllowed();
+    error OnlyRegisteredModulesCanCallThisFunction();
+    error NoZeroDeposits();
+    error NoZeroWithDrawals();
+    error InsufficentFunds();
 
     // --------------------------------------------------------------
     // CONSTRUCTOR
@@ -74,5 +81,37 @@ contract TreasuryModule is Ownable {
         if (_raffleModuleAddress == address(0)) revert ZeroAddressNotAllowed();
 
         raffleModuleAddress = _raffleModuleAddress;
+    }
+
+    // TODO this needs to be called from the raffle on donation
+    function processDonationFromRaffle(uint256 raffleID, uint256 amount)
+        external
+    {
+        if (msg.sender != raffleModuleAddress)
+            revert OnlyRegisteredModulesCanCallThisFunction();
+        require(USDC.transfer(address(this), amount), "DONATION FAILED");
+
+        // TODO need to check how the protocol fees will be taken
+        totaldonationsPerRaffle[raffleID] += amount;
+    }
+
+    function withdrawFundsToOrganisationWallet(uint256 amount)
+        public
+        onlyOwner
+    {
+        if (USDC.balanceOf(address(this)) < amount) revert InsufficentFunds();
+    }
+
+    // ** AAVE DEPOSIT AND WITHDRAWAL ** //
+
+    function depositToAave(uint256 amount) public onlyOwner {
+        if (amount > 0) revert NoZeroDeposits();
+        if (USDC.balanceOf(address(this)) < amount) revert InsufficentFunds();
+        AaveLendingPool.deposit(USDCAddress, amount, address(this), 0);
+    }
+
+    function withdrawFromAave(uint256 amount) public onlyOwner {
+        if (amount > 0) revert NoZeroWithDrawals();
+        AaveLendingPool.withdraw(USDCAddress, amount, address(this));
     }
 }
